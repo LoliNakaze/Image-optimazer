8f05722e6a1dff571e3ebaf40fb9f2798b1e9b45
Karim Hamdi <kaki131@hotmail.fr>
2016-04-22 09:29:21 +0200

    Fusion avec Chang

--- src/fr/umlv/graph/Graph.java
+++ src/fr/umlv/graph/Graph.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 public class Graph {
 	private final ArrayList<ArrayList<Edge>> adjacenyList;
+
+	private static final int INFINI = 256;
 	
 	public Graph(int vertexCount) {
 		adjacenyList = new ArrayList<>(vertexCount);
@@ -40,6 +42,23 @@ public class Graph {
 		return list;
 	}
 	
+	public Graph toGraph (int [][] itr){
+		int i;
+		int line, col;
+		
+		line = itr.length;
+		col = itr[0].length;
+		Graph g = new Graph (line * col + 2); // on veut un sommet par pixel donc on calcule le nombre de pixel avec itr et on ajoute les deux sommets s et t
+		for (i = 0; i < col; i++){
+			g.addEdge(new Edge(0, i+1, INFINI , 0));
+			g.addEdge(new Edge(i+1+(line*(line-1)), g.vertices(), itr[line - 1][i], 0));
+		}
+		
+		for (i = 0; i < col; i++)
+			g.addEdge(new Edge(i + 1, g.vertices() - 1), itr[]);
+		return g;
+	}
+	edge  
 	public void writeFile(Path path) throws IOException {
 		try(BufferedWriter writer = Files.newBufferedWriter(path);
 			PrintWriter printer = new PrintWriter(writer)) {
